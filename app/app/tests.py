d3393584f07010434cebae6cb1853ce6f8ee0de3
unknown <mortenviken@yahoo.com>
2023-05-31 21:27:06 +0200

    Fixed some indent and other typos

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,17 +6,18 @@ from django.test import SimpleTestCase
 from app import calc
 
 
-class CalcTest(SimpleTestCase):
-    """Test the calc module"""
-    
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+ 
     def test_add_numbers(self):
-        """Test adding numbers together"""
+        """Test adding numbers together."""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+ 
+ 
     def test_subtract_numbers(self):
-        """Test Subtracting numbers"""
+        """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
